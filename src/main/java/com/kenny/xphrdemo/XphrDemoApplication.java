a3dbd068011982db4c1fda23c789d0d2ba82ea85
Kenny Ngo <kenny.ngo@bancstac.com>
2025-08-22 08:57:33 +0700

    configure jsp view

--- src/main/java/com/kenny/xphrdemo/XphrDemoApplication.java
+++ src/main/java/com/kenny/xphrdemo/XphrDemoApplication.java
@@ -2,9 +2,16 @@ package com.kenny.xphrdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class XphrDemoApplication {
+public class XphrDemoApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(XphrDemoApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(XphrDemoApplication.class, args);
