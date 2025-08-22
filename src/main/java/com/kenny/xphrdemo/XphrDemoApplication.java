493f3f7057ca4f6029fbf035fef7dd8c4fd9d172
Kenny Ngo <khoa.developer96@gmail.com>
2025-08-22 08:31:10 +0700

    Initialize project

---
+++ src/main/java/com/kenny/xphrdemo/XphrDemoApplication.java
@@ -0,0 +1,13 @@
+package com.kenny.xphrdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XphrDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XphrDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kenny/xphrdemo/XphrDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kenny.xphrdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XphrDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
